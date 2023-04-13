7ccf6e80923263d4c26b6282983c47283923ac8a
fabdennaji <abdennaji.firas2016@gmail.com>
2023-04-13 18:26:27 +0100

    commande

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,178 +1,183 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 
 import 'hammerjs';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { TranslateModule } from '@ngx-translate/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {TranslateModule} from '@ngx-translate/core';
 
-import { CoreModule } from '@core/core.module';
-import { CoreCommonModule } from '@core/common.module';
-import { CoreSidebarModule, CoreThemeCustomizerModule } from '@core/components';
+import {CoreModule} from '@core/core.module';
+import {CoreCommonModule} from '@core/common.module';
+import {CoreSidebarModule, CoreThemeCustomizerModule} from '@core/components';
 
-import { coreConfig } from 'app/app-config';
+import {coreConfig} from 'app/app-config';
 
-import { AppComponent } from 'app/app.component';
-import { LayoutModule } from 'app/layout/layout.module';
-import { SampleModule } from 'app/main/sample/sample.module';
-import { NgxPaginationModule } from 'ngx-pagination'; 
+import {AppComponent} from 'app/app.component';
+import {LayoutModule} from 'app/layout/layout.module';
+import {SampleModule} from 'app/main/sample/sample.module';
+import {NgxPaginationModule} from 'ngx-pagination';
 
-import { JwtInterceptor, ErrorInterceptor } from 'app/auth/helpers';
+import {JwtInterceptor, ErrorInterceptor} from 'app/auth/helpers';
 
-import { CaisseComponent } from './components/caisse/caisse/caisse.component';
-import { AjoutercategorieComponent } from './components/categorie/ajoutercategorie/ajoutercategorie.component';
-import { ListercategorieComponent } from './components/categorie/listercategorie/listercategorie.component';
-import { AffichercategorieComponent } from './components/categorie/affichercategorie/affichercategorie.component';
-import { AjouterfournisseurComponent } from './components/fournisseur/ajouterfournisseur/ajouterfournisseur.component';
-import { ListerfournisseurComponent } from './components/fournisseur/listerfournisseur/listerfournisseur.component';
-import { AfficherfournisseurComponent } from './components/fournisseur/afficherfournisseur/afficherfournisseur.component';
-import { AjouteremployeComponent } from './components/employe/ajouteremploye/ajouteremploye.component';
-import { ListeremployeComponent } from './components/employe/listeremploye/listeremploye.component';
-import { AfficheremployeComponent } from './components/employe/afficheremploye/afficheremploye.component';
-import { ListerfactureComponent } from './components/facture/listerfacture/listerfacture.component';
-import { AfficherfactureComponent } from './components/facture/afficherfacture/afficherfacture.component';
-import { PayercreditComponent } from './components/credit/payercredit/payercredit.component';
-import { AjouterclientComponent } from './components/client/ajouterclient/ajouterclient.component';
-import { ListerclientComponent } from './components/client/listerclient/listerclient.component';
-import { AfficherclientComponent } from './components/client/afficherclient/afficherclient.component';
-import { AjouterfermetureCaisseComponent } from './components/fermetureCaisse/ajouterfermeture-caisse/ajouterfermeture-caisse.component';
-import { ListerfermetureCaisseComponent } from './components/fermetureCaisse/listerfermeture-caisse/listerfermeture-caisse.component';
-import { AfficherfermetureCaisseComponent } from './components/fermetureCaisse/afficherfermeture-caisse/afficherfermeture-caisse.component';
-import { AjouterproduitComponent } from './components/produit/ajouterproduit/ajouterproduit.component';
-import { ListerproduitComponent } from './components/produit/listerproduit/listerproduit.component';
-import { AfficherproduitComponent } from './components/produit/afficherproduit/afficherproduit.component';
-import { ModifierclientComponent } from './components/client/modifierclient/modifierclient.component';
-import { ModifiercategorieComponent } from './components/categorie/modifiercategorie/modifiercategorie.component';
-import { ModifierproduitComponent } from './components/produit/modifierproduit/modifierproduit.component';
-import { UserProfileComponent } from './components/profile/user-profile/user-profile.component';
-import { ChangePasswordComponent } from './components/profile/change-password/change-password.component';
+import {CaisseComponent} from './components/caisse/caisse/caisse.component';
+import {AjoutercategorieComponent} from './components/categorie/ajoutercategorie/ajoutercategorie.component';
+import {ListercategorieComponent} from './components/categorie/listercategorie/listercategorie.component';
+import {AffichercategorieComponent} from './components/categorie/affichercategorie/affichercategorie.component';
+import {AjouterfournisseurComponent} from './components/fournisseur/ajouterfournisseur/ajouterfournisseur.component';
+import {ListerfournisseurComponent} from './components/fournisseur/listerfournisseur/listerfournisseur.component';
+import {AfficherfournisseurComponent} from './components/fournisseur/afficherfournisseur/afficherfournisseur.component';
+import {AjouteremployeComponent} from './components/employe/ajouteremploye/ajouteremploye.component';
+import {ListeremployeComponent} from './components/employe/listeremploye/listeremploye.component';
+import {AfficheremployeComponent} from './components/employe/afficheremploye/afficheremploye.component';
+import {ListerfactureComponent} from './components/facture/listerfacture/listerfacture.component';
+import {AfficherfactureComponent} from './components/facture/afficherfacture/afficherfacture.component';
+import {PayercreditComponent} from './components/credit/payercredit/payercredit.component';
+import {AjouterclientComponent} from './components/client/ajouterclient/ajouterclient.component';
+import {ListerclientComponent} from './components/client/listerclient/listerclient.component';
+import {AfficherclientComponent} from './components/client/afficherclient/afficherclient.component';
+import {
+    AjouterfermetureCaisseComponent
+} from './components/fermetureCaisse/ajouterfermeture-caisse/ajouterfermeture-caisse.component';
+import {
+    ListerfermetureCaisseComponent
+} from './components/fermetureCaisse/listerfermeture-caisse/listerfermeture-caisse.component';
+import {
+    AfficherfermetureCaisseComponent
+} from './components/fermetureCaisse/afficherfermeture-caisse/afficherfermeture-caisse.component';
+import {AjouterproduitComponent} from './components/produit/ajouterproduit/ajouterproduit.component';
+import {ListerproduitComponent} from './components/produit/listerproduit/listerproduit.component';
+import {AfficherproduitComponent} from './components/produit/afficherproduit/afficherproduit.component';
+import {ModifierclientComponent} from './components/client/modifierclient/modifierclient.component';
+import {ModifiercategorieComponent} from './components/categorie/modifiercategorie/modifiercategorie.component';
+import {ModifierproduitComponent} from './components/produit/modifierproduit/modifierproduit.component';
+import {UserProfileComponent} from './components/profile/user-profile/user-profile.component';
+import {ChangePasswordComponent} from './components/profile/change-password/change-password.component';
 
-import { HomeshopComponent } from './layout/homeshop/homeshop.component';
-import { PannierComponent } from './layout/pannier/pannier.component';
+import {HomeshopComponent} from './layout/homeshop/homeshop.component';
+import {PannierComponent} from './layout/pannier/pannier.component';
 import {CoreCardModule} from '../@core/components/core-card/core-card.module';
 import {NgxBarcodeModule} from 'ngx-barcode';
 import {NgxPrintModule} from 'ngx-print';
 import {LocalStorageService} from 'ngx-webstorage';
 import {NgSelectModule} from '@ng-select/ng-select';
+import {CommandesComponent} from './layout/commandes/commandes.component';
 
 const appRoutes: Routes = [
 
 
-  {
-    path:'modifier-password',component:ChangePasswordComponent
-  },
-  {
-    path:'user-profile',component:UserProfileComponent
-  },
-{
-    path:'caisse',component:CaisseComponent
-  },
+    {
+        path: 'modifier-password', component: ChangePasswordComponent
+    },
+    {
+        path: 'user-profile', component: UserProfileComponent
+    },
+    {
+        path: 'caisse', component: CaisseComponent
+    },
 
-  {
-    path:'facture/lister',component:ListerfactureComponent
-  },
-  {
-    path:'afficherfactureclient/:id',component:AfficherfactureComponent
-  },
-
-
-  {
-    path:'credit/payer',component:PayercreditComponent
-  },
-  {
-    path:'client/ajouter',component:AjouterclientComponent
-  },
-  {
-    path: 'afficherclient/:id',component: AfficherclientComponent
-  },
-  {
-    path: 'modifierclient/:id',component: ModifierclientComponent
-  },
-
-  {
-    path:'client/lister',component:ListerclientComponent
-  },
-  {
-    path:'fermeturecaisse/ajouter',component:AjouterfermetureCaisseComponent
-  },
-  {
-    path:'fermeturecaisse/lister',component:ListerfermetureCaisseComponent
-  },
-  {
-    path:'produit/ajouter',component:AjouterproduitComponent
-  },
-  {
-    path:'produit/lister',component:ListerproduitComponent
-  },
-  {
-    path: 'afficherproduit/:id',component: AfficherproduitComponent
-  },
-  {
-    path: 'modifierproduit/:id',component: ModifierproduitComponent
-  },
-
-  {
-    path:'categorie/ajouter',component:AjoutercategorieComponent
-  },
-  {
-    path:'categorie/lister',component:ListercategorieComponent
-  },
-  {
-    path:'fournisseur/ajouter',component:AjouterfournisseurComponent
-  },
-  {
-    path:'fournisseur/lister',component:ListerfournisseurComponent
-  },
-  {
-    path: 'affichercategorie/:id',component: AffichercategorieComponent
-  },
-  {
-    path: 'modifiercategorie/:id',component: ModifiercategorieComponent
-  },
-  {
-    path:'homeshop',component:HomeshopComponent
-  },
-  {
-    path:'pannier',component:PannierComponent
-  },
+    {
+        path: 'facture/lister', component: ListerfactureComponent
+    },
+    {
+        path: 'afficherfactureclient/:id', component: AfficherfactureComponent
+    },
 
 
-  
+    {
+        path: 'credit/payer', component: PayercreditComponent
+    },
+    {
+        path: 'client/ajouter', component: AjouterclientComponent
+    },
+    {
+        path: 'afficherclient/:id', component: AfficherclientComponent
+    },
+    {
+        path: 'modifierclient/:id', component: ModifierclientComponent
+    },
 
-  {
-    path:'employe/ajouter',component:AjouteremployeComponent
-  },
-  {
-    path:'employe/lister',component:ListeremployeComponent
-  },
-  {
-    path:'employe/afficher',component:AfficheremployeComponent
-  },
+    {
+        path: 'client/lister', component: ListerclientComponent
+    },
+    {
+        path: 'fermeturecaisse/ajouter', component: AjouterfermetureCaisseComponent
+    },
+    {
+        path: 'fermeturecaisse/lister', component: ListerfermetureCaisseComponent
+    },
+    {
+        path: 'produit/ajouter', component: AjouterproduitComponent
+    },
+    {
+        path: 'produit/lister', component: ListerproduitComponent
+    },
+    {
+        path: 'afficherproduit/:id', component: AfficherproduitComponent
+    },
+    {
+        path: 'modifierproduit/:id', component: ModifierproduitComponent
+    },
 
- 
-  
+    {
+        path: 'categorie/ajouter', component: AjoutercategorieComponent
+    },
+    {
+        path: 'categorie/lister', component: ListercategorieComponent
+    },
+    {
+        path: 'fournisseur/ajouter', component: AjouterfournisseurComponent
+    },
+    {
+        path: 'fournisseur/lister', component: ListerfournisseurComponent
+    },
+    {
+        path: 'affichercategorie/:id', component: AffichercategorieComponent
+    },
+    {
+        path: 'modifiercategorie/:id', component: ModifiercategorieComponent
+    },
+    {
+        path: 'homeshop', component: HomeshopComponent
+    },
+    {
+        path: 'commandes', component: CommandesComponent
+    },
+    {
+        path: 'pannier', component: PannierComponent
+    },
 
 
-  {
-    path: 'pages',
-    loadChildren: () => import('./main/pages/pages.module').then(m => m.PagesModule)
-  },
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-  {
-    path: '**',
-    redirectTo: '/pages/miscellaneous/error' //Error 404 - Page not found
-  }
+    {
+        path: 'employe/ajouter', component: AjouteremployeComponent
+    },
+    {
+        path: 'employe/lister', component: ListeremployeComponent
+    },
+    {
+        path: 'employe/afficher', component: AfficheremployeComponent
+    },
+
+
+    {
+        path: 'pages',
+        loadChildren: () => import('./main/pages/pages.module').then(m => m.PagesModule)
+    },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: '/pages/miscellaneous/error' //Error 404 - Page not found
+    }
 ];
 
 @NgModule({
-  declarations: [AppComponent,
-  CaisseComponent,
+    declarations: [AppComponent,
+        CaisseComponent,
         AjoutercategorieComponent,
         ListercategorieComponent,
         AffichercategorieComponent,
@@ -229,12 +234,13 @@ const appRoutes: Routes = [
         CoreCardModule,
         NgSelectModule
     ],
-  providers: [
-      LocalStorageService,
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-],
-  bootstrap: [AppComponent]
+    providers: [
+        LocalStorageService,
+        {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+        {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
 
